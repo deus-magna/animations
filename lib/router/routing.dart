8ad0bc6750f611373562b440923c9e6fc07e2a46
Renato <52421382+deus-magna@users.noreply.github.com>
2021-05-19 23:47:23 -0600

    Adding the sin wave view to the routing file

--- lib/router/routing.dart
+++ lib/router/routing.dart
@@ -2,6 +2,7 @@ import 'package:animations/views/animated_triangle_view.dart';
 import 'package:animations/views/circles_view.dart';
 import 'package:animations/views/home_view.dart';
 import 'package:animations/views/multiple_waves_view.dart';
+import 'package:animations/views/sine_wave_view.dart';
 import 'package:animations/views/waves_view.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     '/circles': (BuildContext context) => CirclesView(),
     '/waves': (BuildContext context) => WavesView(),
     '/multi_waves': (BuildContext context) => MultipleWavesView(),
+    '/sin': (BuildContext context) => SinWaveView(),
     '/home': (BuildContext context) => HomeView(),
   };
 }
