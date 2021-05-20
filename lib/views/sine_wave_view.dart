fc0c86038ea69b173f972dd0cbc37c463686c9e5
Renato <52421382+deus-magna@users.noreply.github.com>
2021-05-19 23:57:28 -0600

    Trying to add a blur effect on custom painter

--- lib/views/sine_wave_view.dart
+++ lib/views/sine_wave_view.dart
@@ -8,6 +8,57 @@ class SinWaveView extends StatefulWidget {
 class _SinWaveViewState extends State<SinWaveView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: CustomPaint(
+        painter: SinWavePainter(),
+        child: Container(
+          width: size.width,
+          height: size.height,
+        ),
+      ),
+    );
   }
+}
+
+class SinWavePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.grey
+      ..strokeWidth = 2
+      ..style = PaintingStyle.fill
+      ..strokeCap = StrokeCap.round;
+
+    canvas.drawLine(Offset(0, size.height / 2 - 100),
+        Offset(size.width, size.height / 2 - 100), paint);
+
+    canvas.drawLine(
+        Offset(0, size.height / 2), Offset(size.width, size.height / 2), paint);
+
+    canvas.drawLine(Offset(0, size.height / 2 + 100),
+        Offset(size.width, size.height / 2 + 100), paint);
+
+    paint.color = Colors.black12;
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 61, paint);
+    paint.color = Colors.black26;
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 60, paint);
+    paint.color = Colors.black38;
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 58, paint);
+    paint.color = Colors.black45;
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 56, paint);
+    paint.color = Colors.black54;
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 54, paint);
+    paint.color = Colors.black87;
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 52, paint);
+    paint.color = Colors.black;
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 50, paint);
+  }
+
+  @override
+  bool shouldRepaint(SinWavePainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(SinWavePainter oldDelegate) => false;
 }
