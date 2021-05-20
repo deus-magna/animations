396ff0624a1d152389017c97a0b276bf6ead340e
Renato <52421382+deus-magna@users.noreply.github.com>
2021-05-19 23:46:38 -0600

    Add sin wave view

---
+++ lib/views/sine_wave_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SinWaveView extends StatefulWidget {
+  @override
+  _SinWaveViewState createState() => _SinWaveViewState();
+}
+
+class _SinWaveViewState extends State<SinWaveView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
