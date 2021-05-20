4ea0ab48c35e36cf375a122c825ae828557fc962
Renato <52421382+deus-magna@users.noreply.github.com>
2021-05-20 00:42:01 -0600

    Creating an extension for Canvas to add BluredCircle method

--- lib/views/sine_wave_view.dart
+++ lib/views/sine_wave_view.dart
@@ -40,20 +40,40 @@ class SinWavePainter extends CustomPainter {
     canvas.drawLine(Offset(0, size.height / 2 + 100),
         Offset(size.width, size.height / 2 + 100), paint);
 
-    paint.color = Colors.black12;
-    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 61, paint);
-    paint.color = Colors.black26;
-    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 60, paint);
-    paint.color = Colors.black38;
-    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 58, paint);
-    paint.color = Colors.black45;
-    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 56, paint);
-    paint.color = Colors.black54;
-    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 54, paint);
-    paint.color = Colors.black87;
-    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 52, paint);
-    paint.color = Colors.black;
-    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 50, paint);
+    canvas.drawBluredCircle(
+        Offset(size.width / 2, size.height - 150), 50, paint);
+    drawBluredCircle(canvas, size, 50, 1);
+
+    // paint.color = Colors.black12;
+    // canvas.drawCircle(Offset(size.width / 2, size.height / 2), 61, paint);
+    // paint.color = Colors.black26;
+    // canvas.drawCircle(Offset(size.width / 2, size.height / 2), 60, paint);
+    // paint.color = Colors.black38;
+    // canvas.drawCircle(Offset(size.width / 2, size.height / 2), 58, paint);
+    // paint.color = Colors.black45;
+    // canvas.drawCircle(Offset(size.width / 2, size.height / 2), 56, paint);
+    // paint.color = Colors.black54;
+    // canvas.drawCircle(Offset(size.width / 2, size.height / 2), 54, paint);
+    // paint.color = Colors.black87;
+    // canvas.drawCircle(Offset(size.width / 2, size.height / 2), 52, paint);
+    // paint.color = Colors.black;
+    // canvas.drawCircle(Offset(size.width / 2, size.height / 2), 50, paint);
+  }
+
+  void drawBluredCircle(Canvas canvas, Size size, double radius, int step) {
+    int alpha = 255;
+    final paint = Paint()
+      ..color = Colors.black.withAlpha(alpha)
+      ..strokeWidth = 2
+      ..style = PaintingStyle.fill
+      ..strokeCap = StrokeCap.round;
+
+    for (double i = step.toDouble(); i <= radius; i += step) {
+      alpha = alpha - 5;
+      // print('Radius: $i, alpha: $alpha');
+      paint.color = Colors.black.withAlpha(alpha);
+      canvas.drawCircle(Offset(size.width / 2, size.height / 2), i, paint);
+    }
   }
 
   @override
@@ -61,4 +81,17 @@ class SinWavePainter extends CustomPainter {
 
   @override
   bool shouldRebuildSemantics(SinWavePainter oldDelegate) => false;
+}
+
+extension ExtendedCanvas on Canvas {
+  void drawBluredCircle(Offset offset, double radius, Paint paint) {
+    int step = 1;
+    int alpha = 255;
+    for (int i = 0; i <= radius; i += step) {
+      alpha = alpha - 5;
+      print('Radius: $i, alpha: $alpha');
+      paint.color = Colors.black.withAlpha(alpha);
+      this.drawCircle(offset, i.toDouble(), paint);
+    }
+  }
 }
