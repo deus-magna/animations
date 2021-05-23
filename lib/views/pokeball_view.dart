af5c99c71026b4864cd8f67b8704cd50f9559962
Renato <52421382+deus-magna@users.noreply.github.com>
2021-05-22 23:15:15 -0600

    CAdded drawPokeball method in PokeBallPainter

--- lib/views/pokeball_view.dart
+++ lib/views/pokeball_view.dart
@@ -26,18 +26,24 @@ class _PokeballViewState extends State<PokeballView> {
 class PokeballPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
+    drawPokeball(Offset(100, 100), 50, canvas);
+    drawPokeball(Offset(size.width / 2, size.height / 2), 100, canvas);
+    drawPokeball(Offset(size.width - 100, size.height - 100), 150, canvas);
+  }
+
+  void drawPokeball(Offset center, double radius, Canvas canvas) {
     Paint paint = Paint()..color = Colors.red;
     Paint strokePaint = Paint()
       ..color = Colors.black
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 10;
+      ..strokeWidth = radius * 0.15;
 
     // Draw the red fill semicircle
     canvas.drawArc(
       Rect.fromCenter(
-        center: Offset(size.width / 2, size.height / 2),
-        height: 200,
-        width: 200,
+        center: center,
+        height: radius * 2,
+        width: radius * 2,
       ),
       math.pi,
       math.pi,
@@ -48,19 +54,22 @@ class PokeballPainter extends CustomPainter {
     paint = Paint()..color = Colors.grey[200];
     canvas.drawArc(
       Rect.fromCenter(
-        center: Offset(size.width / 2, size.height / 2),
-        height: 200,
-        width: 200,
+        center: center,
+        height: radius * 2,
+        width: radius * 2,
       ),
       0,
       math.pi,
       false,
       paint,
     );
-    canvas.drawLine(Offset(size.width / 2 - 100, size.height / 2),
-        Offset(size.width / 2 + 100, size.height / 2), strokePaint);
-    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 20, paint);
-    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 20, strokePaint);
+    canvas.drawLine(Offset(center.dx - radius, center.dy),
+        Offset(center.dx + radius, center.dy), strokePaint);
+    canvas.drawCircle(center, radius / 4, paint);
+    canvas.drawCircle(center, radius / 4, strokePaint);
+
+    strokePaint.strokeWidth = 2;
+    canvas.drawCircle(center, radius, strokePaint);
   }
 
   @override
