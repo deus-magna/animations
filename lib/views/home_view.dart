38362c806a5b9065e0e170416ef3423c7f32dd3a
Renato <52421382+deus-magna@users.noreply.github.com>
2021-05-22 22:48:18 -0600

    Reduce the size of the arrow in home view, and make a const widget for it

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const arrow = Icon(Icons.arrow_forward_ios_rounded, size: 16);
     return Scaffold(
       appBar: AppBar(
         title: Text('Animations'),
@@ -12,37 +13,37 @@ class HomeView extends StatelessWidget {
           ListTile(
             leading: Icon(Icons.category),
             title: Text('Animated Triangle'),
-            trailing: Icon(Icons.arrow_forward_ios_rounded),
+            trailing: arrow,
             onTap: () => Navigator.of(context).pushNamed('/triangle'),
           ),
           ListTile(
             leading: Icon(Icons.waves),
             title: Text('Circular Waves'),
-            trailing: Icon(Icons.arrow_forward_ios_rounded),
+            trailing: arrow,
             onTap: () => Navigator.of(context).pushNamed('/waves'),
           ),
           ListTile(
             leading: Icon(Icons.waves),
             title: Text('Multiple Waves'),
-            trailing: Icon(Icons.arrow_forward_ios_rounded),
+            trailing: arrow,
             onTap: () => Navigator.of(context).pushNamed('/multi_waves'),
           ),
           ListTile(
             leading: Icon(Icons.waves),
             title: Text('Circles'),
-            trailing: Icon(Icons.arrow_forward_ios_rounded),
+            trailing: arrow,
             onTap: () => Navigator.of(context).pushNamed('/circles'),
           ),
           ListTile(
             leading: Icon(Icons.waves),
             title: Text('Sin Wave'),
-            trailing: Icon(Icons.arrow_forward_ios_rounded),
+            trailing: arrow,
             onTap: () => Navigator.of(context).pushNamed('/sin'),
           ),
           ListTile(
             leading: Icon(Icons.sports_baseball_sharp),
             title: Text('Pokeball'),
-            trailing: Icon(Icons.arrow_forward_ios_rounded),
+            trailing: arrow,
             onTap: () => Navigator.of(context).pushNamed('/pokeball'),
           )
         ],
