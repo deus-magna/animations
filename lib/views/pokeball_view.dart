cf61ae4da54d9ea2b3a216c9d411234e31bfda2e
Renato <52421382+deus-magna@users.noreply.github.com>
2021-05-22 23:02:35 -0600

    Creating PokeballPainter

--- lib/views/pokeball_view.dart
+++ lib/views/pokeball_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class PokeballView extends StatefulWidget {
   @override
@@ -24,7 +25,43 @@ class _PokeballViewState extends State<PokeballView> {
 
 class PokeballPainter extends CustomPainter {
   @override
-  void paint(Canvas canvas, Size size) {}
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = Colors.red;
+    Paint strokePaint = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 10;
+
+    // Draw the red fill semicircle
+    canvas.drawArc(
+      Rect.fromCenter(
+        center: Offset(size.width / 2, size.height / 2),
+        height: 200,
+        width: 200,
+      ),
+      math.pi,
+      math.pi,
+      false,
+      paint,
+    );
+
+    paint = Paint()..color = Colors.grey[200];
+    canvas.drawArc(
+      Rect.fromCenter(
+        center: Offset(size.width / 2, size.height / 2),
+        height: 200,
+        width: 200,
+      ),
+      0,
+      math.pi,
+      false,
+      paint,
+    );
+    canvas.drawLine(Offset(size.width / 2 - 100, size.height / 2),
+        Offset(size.width / 2 + 100, size.height / 2), strokePaint);
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 20, paint);
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 20, strokePaint);
+  }
 
   @override
   bool shouldRepaint(PokeballPainter oldDelegate) => false;
